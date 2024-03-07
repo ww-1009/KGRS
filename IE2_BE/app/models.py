fb78f9e1c0db114bdb57d745459436f29af58a0d
吴维 <wei.wu@innodealing.com>
2024-03-07 11:11:26 +0800

    new

--- IE2_BE/app/models.py
+++ IE2_BE/app/models.py
@@ -3,50 +3,96 @@ from django.db import models
 
 # Create your models here.
 class FdClassForest(models.Model):
-	class_name = models.CharField(max_length=255, blank=True, null=True)
-	sup = models.CharField(max_length=255, blank=True, null=True)
-	sub = models.TextField(blank=True, null=True)
-	info = models.CharField(max_length=255, blank=True, null=True)
+    class_name = models.CharField(max_length=255, blank=True, null=True)
+    sup = models.CharField(max_length=255, blank=True, null=True)
+    sub = models.TextField(blank=True, null=True)
+    info = models.CharField(max_length=255, blank=True, null=True)
 
-	class Meta:
-		managed = False
-		db_table = 'fd_class_forest'
+    class Meta:
+        managed = False
+        db_table = 'fd_class_forest'
 
 
 class FdEntity(models.Model):
-	id = models.IntegerField(primary_key=True)
-	entity = models.CharField(max_length=100)
-	imgurl = models.CharField(db_column='imgUrl', max_length=255, blank=True, null=True)  # Field name made lowercase.
-	relatedtype = models.TextField(db_column='relatedType', blank=True, null=True)  # Field name made lowercase.
-	abstract = models.TextField(blank=True, null=True)
+    id = models.IntegerField(primary_key=True)
+    entity = models.CharField(max_length=100)
+    imgurl = models.CharField(db_column='imgUrl', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    relatedtype = models.TextField(db_column='relatedType', blank=True, null=True)  # Field name made lowercase.
+    abstract = models.TextField(blank=True, null=True)
 
-	class Meta:
-		managed = False
-		db_table = 'fd_entity'
+    class Meta:
+        managed = False
+        db_table = 'fd_entity'
 
 
 class FdRelation(models.Model):
-	id_s = models.IntegerField(db_column='id_S', primary_key=True)  # Field name made lowercase.
-	s = models.CharField(db_column='S', max_length=100)  # Field name made lowercase.
-	p = models.CharField(db_column='P', max_length=100)  # Field name made lowercase.
-	o = models.CharField(db_column='O', max_length=100)  # Field name made lowercase.
-	id_o = models.IntegerField(db_column='id_O')  # Field name made lowercase.
+    id_s = models.IntegerField(db_column='id_S', primary_key=True)  # Field name made lowercase.
+    s = models.CharField(db_column='S', max_length=100)  # Field name made lowercase.
+    p = models.CharField(db_column='P', max_length=100)  # Field name made lowercase.
+    o = models.CharField(db_column='O', max_length=100)  # Field name made lowercase.
+    id_o = models.IntegerField(db_column='id_O')  # Field name made lowercase.
 
-	class Meta:
-		managed = False
-		db_table = 'fd_relation'
-		unique_together = (('id_s', 'id_o'),)
+    class Meta:
+        managed = False
+        db_table = 'fd_relation'
+        unique_together = (('id_s', 'id_o'),)
+
+
+class SelfEntity(models.Model):
+    entity = models.CharField(max_length=255, blank=True, null=True)
+    imgurl = models.CharField(db_column='imgUrl', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    relatedtype = models.CharField(db_column='relatedType', max_length=255, blank=True,
+                                   null=True)  # Field name made lowercase.
+    abstract = models.CharField(max_length=255, blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    user_id = models.IntegerField()
+    graph_id = models.IntegerField()
+    entity_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'self.entity'
+
+
+class SelfRelation(models.Model):
+    user_id = models.IntegerField()
+    graph_id = models.IntegerField()
+    id_s = models.IntegerField(db_column='id_S', blank=True, null=True)  # Field name made lowercase.
+    s = models.CharField(db_column='S', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    p = models.CharField(db_column='P', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    o = models.CharField(db_column='O', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    id_o = models.IntegerField(db_column='id_O', blank=True, null=True)  # Field name made lowercase.
+    update_time = models.DateTimeField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'self.relation'
+
+
+class SelfGraphInfo(models.Model):
+    user_id = models.IntegerField()
+    graph_id = models.IntegerField()
+    graph_name = models.CharField(max_length=255, blank=True, null=True)
+    deleted = models.IntegerField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'self_graph_info'
 
 
 class User(models.Model):
-	id = models.IntegerField(primary_key=True)
-	user_name = models.CharField(max_length=20, blank=True, null=True)
-	password = models.CharField(max_length=20, blank=True, null=True)
-	phone = models.CharField(max_length=11, blank=True, null=True)
-	email = models.CharField(max_length=30, blank=True, null=True)
-	create_time = models.DateTimeField(blank=True, null=True)
-	update_time = models.DateTimeField(blank=True, null=True)
+    id = models.IntegerField(primary_key=True)
+    user_name = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
+    email = models.CharField(max_length=30, blank=True, null=True)
+    create_time = models.DateTimeField(blank=True, null=True)
+    update_time = models.DateTimeField(blank=True, null=True)
 
-	class Meta:
-		managed = False
-		db_table = 'user'
+    class Meta:
+        managed = False
+        db_table = 'user'
--- IE2_BE/app/views.py
+++ IE2_BE/app/views.py
@@ -1,11 +1,11 @@
 import json
 
 from django.core.serializers import serialize
-from django.db.models import Q
+from django.db.models import Q, Max
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from app.models import FdEntity, FdRelation
+from app.models import FdEntity, FdRelation, SelfGraphInfo, SelfRelation, SelfEntity
 
 
 # Create your views here.
@@ -19,7 +19,7 @@ def get_fd_group(request):
 	node_map = {}
 	try:
 		inputstr = data['inputstr']
-		fd_entity = FdEntity.objects.get(entity=inputstr)  # 查询指定ID的用户
+		fd_entity = FdEntity.objects.get(entity=inputstr)
 		fd_entity_data = {'id': fd_entity.id, 'entity': fd_entity.entity,
 						  'img_url': fd_entity.imgurl, 'relatedType': fd_entity.relatedtype,
 						  'abstract': fd_entity.abstract}
@@ -31,20 +31,66 @@ def get_fd_group(request):
 			# relation_temp = {"source": fd_relation_data["id_s"], "target": fd_relation_data["id_o"], "category": 0, "value": fd_relation_data["p"], "symbolSize": 10}
 			entity_relation.append({"source": fd_relation_data["id_s"], "target": fd_relation_data["id_o"],
 									"category": 0, "value": fd_relation_data["p"], "symbolSize": 10})
-			node_map[fd_relation_data["id_s"]] = {"name": fd_relation_data["s"], "category": 1, "id": fd_relation_data["id_s"]}
-			node_map[fd_relation_data["id_o"]] = {"name": fd_relation_data["o"], "category": 1, "id": fd_relation_data["id_o"]}
-			# entity_node.append({"name": fd_relation_data["s"], "category": 1, "id": fd_relation_data["id_s"]})
-			# entity_node.append({"name": fd_relation_data["o"], "category": 1, "id": fd_relation_data["id_o"]})
+			node_map[fd_relation_data["id_s"]] = {"name": fd_relation_data["s"], "category": 1,
+												  "id": fd_relation_data["id_s"]}
+			node_map[fd_relation_data["id_o"]] = {"name": fd_relation_data["o"], "category": 1,
+												  "id": fd_relation_data["id_o"]}
+		# entity_node.append({"name": fd_relation_data["s"], "category": 1, "id": fd_relation_data["id_s"]})
+		# entity_node.append({"name": fd_relation_data["o"], "category": 1, "id": fd_relation_data["id_o"]})
 		# fd_reelation_json = serialize("json", fd_relation) # 转化为json对象
 		node_map[fd_entity.id] = {"name": fd_entity.entity, "category": 0, "id": fd_entity.id}
 		for value in node_map.values():
 			entity_node.append(value)
 		print(entity_node)
 		print(entity_relation)
-		# entity_node=[{'name': 'Elon Musk', 'category': 0, 'id': 0}, {'name': 'Bel Air, Los Angeles', 'category': 1, 'id': 1}]
-		# entity_relation=[{'source': 0, 'target': 1, 'category': 0, 'value': 'residence', 'symbolSize': 10}]
+	# entity_node=[{'name': 'Elon Musk', 'category': 0, 'id': 0}, {'name': 'Bel Air, Los Angeles', 'category': 1, 'id': 1}]
+	# entity_relation=[{'source': 0, 'target': 1, 'category': 0, 'value': 'residence', 'symbolSize': 10}]
 	except Exception as e:
 		print(f"出错了{e}")
 		return JsonResponse({'code': 0, 'msg': "查询inputstr出现异常，具体错误：" + str(e)})
 
 	return JsonResponse({'code': 200, 'entity_node': entity_node, 'entity_relation': entity_relation})
+
+
+def new_self_graph(request):
+	data = json.loads(request.body.decode('utf-8'))
+	user_id = data['user_id']
+	graph_name = data['graph_name']
+	try:
+		# 获取number_field字段的最大值
+		graph_id_max = SelfGraphInfo.objects.aggregate(Max('graph_id'))['number_field__max']
+
+		if graph_id_max is None:
+			graph_id_max = -1  # 如果表为空，可以设置一个默认值
+		graph_id = graph_id_max + 1
+		new_self_graph_info = SelfGraphInfo(user_id=user_id, graph_name=graph_name, graph_id=graph_id, deleted=0)
+		new_self_graph_info.save()
+	except Exception as e:
+		print(f"出错了{e}")
+		return JsonResponse({'code': 0, 'msg': "查询inputstr出现异常，具体错误：" + str(e)})
+
+	return JsonResponse({'code': 200, 'graph_id': graph_id})
+
+
+def get_self_group_info(request):
+	data = json.loads(request.body.decode('utf-8'))
+	user_id = data["user_id"]
+	try:
+		self_group_info_data = SelfGraphInfo.objects.filter(user_id=user_id, deleted=0)
+	except Exception as e:
+		print(f"出错了{e}")
+		return JsonResponse({'code': 0, 'msg': "获取自有图谱信息出现异常，具体错误：" + str(e)})
+def get_self_group(request):
+	data = json.loads(request.body.decode('utf-8'))
+	user_id = data["user_id"]
+	graph_id = data["graph_id"]
+	try:
+		self_entity_data = SelfEntity.objects.filter(user_id=user_id, graph_id=graph_id)
+		for item in self_entity_data:
+			print(item.entity, item.relatedtype, item.abstract)
+
+		self_relation_data = SelfRelation.objects.filter(user_id=user_id, graph_id=graph_id)
+
+	except Exception as e:
+		print(f"出错了{e}")
+		return JsonResponse({'code': 0, 'msg': "获取自有图谱出现异常，具体错误：" + str(e)})
